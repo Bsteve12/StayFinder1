802b6298da37fab9a558170f48136b8673285b28
Allis21 <allishincapie21@gmail.com>
2025-11-10 17:23:40 -0500

    creacion componentes

---
+++ frontend/src/app/anfitrion/anfitrion.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anfitrion',
+  imports: [],
+  templateUrl: './anfitrion.html',
+  styleUrl: './anfitrion.scss',
+})
+export class Anfitrion {
+
+}
---
+++ frontend/src/app/convertirse-anfitrion/convertirse-anfitrion.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-convertirse-anfitrion',
+  imports: [],
+  templateUrl: './convertirse-anfitrion.html',
+  styleUrl: './convertirse-anfitrion.scss',
+})
+export class ConvertirseAnfitrion {
+
+}
